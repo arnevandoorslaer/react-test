cd1272c78864fb1d4b39d4d70dde2faee2c39725
arnevandoorslaer <vando@telenet.be>
2023-10-04 22:58:54 +0200

    chore: remove action component

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,7 +1,6 @@
-import ActionComponent from './action-component';
 import FormComponent from './form-components/form-component';
 import FormFieldComponent from './form-components/form-field-component';
 import FormCreateComponent from './form-components/form-create-component';
 import FormUpdateComponent from './form-components/form-update-component';
 
-export { ActionComponent, FormComponent, FormFieldComponent, FormCreateComponent, FormUpdateComponent };
+export { FormComponent, FormFieldComponent, FormCreateComponent, FormUpdateComponent };
